3f4d66340cc869b01839299a1009f78d89009db9
Ing. Maryan Espinoza <analista.guma@gmail.com>
2024-01-23 09:50:58 -0600

    Produccion

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
     $host       = "mysql_server";
     $user       = "root";
     $pass       = "a7m1425.";
-    $database   = "gmv3_inn"; 
+    $database   = "db_preventa_inn"; 
 
     $connect = new mysqli($host, $user, $pass, $database);
     if (!$connect) {
